db74899ca2a4baccaaca4bce164bf64f648e9d5e
shengbinmeng <shengbinmeng@gmail.com>
2013-06-07 21:43:26 +0800

    assign readable ids; add buton event listeners

--- IPGateway/src/pku/shengbin/ipgateway/MainActivity.java
+++ IPGateway/src/pku/shengbin/ipgateway/MainActivity.java
@@ -3,6 +3,11 @@ package pku.shengbin.ipgateway;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +15,29 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        EditText messageArea = (EditText) this.findViewById(R.id.editText_message);
+        messageArea.setText(R.string.welcome);
+        
+        Button loginButton = (Button) this.findViewById(R.id.button_login);
+        loginButton.setOnClickListener(new OnClickListener(){
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Toast.makeText(MainActivity.this, "Login", Toast.LENGTH_SHORT).show();
+			}
+        	
+        });
+        
+        Button logoutButton = (Button) this.findViewById(R.id.button_logout);
+        logoutButton.setOnClickListener(new OnClickListener(){
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Toast.makeText(MainActivity.this, "Logout", Toast.LENGTH_SHORT).show();
+			}
+        	
+        });
     }
 
 

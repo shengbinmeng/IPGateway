a7d507825bc5218c33d5aec8b4e251a044a1d9cd
Shengbin Meng <shengbinmeng@gmail.com>
2014-10-27 19:08:44 +0800

    add the feature of disconnect all

--- IPGateway/src/pku/shengbin/ipgateway/MainActivity.java
+++ IPGateway/src/pku/shengbin/ipgateway/MainActivity.java
@@ -72,6 +72,13 @@ public class MainActivity extends Activity {
 				doLogout();
 			}
         });
+        Button logoutAllButton = (Button) this.findViewById(R.id.button_logout_all);
+        logoutAllButton.setOnClickListener(new OnClickListener(){
+			@Override
+			public void onClick(View v) {
+				doLogoutAll();
+			}
+        });
     }
 
     
@@ -97,7 +104,6 @@ public class MainActivity extends Activity {
 	
     @Override
 	public boolean onOptionsItemSelected(MenuItem item) {
-		// TODO Auto-generated method stub
     	if (item.getItemId() == R.id.action_about) {
     		//MessageBox.show(this, this.getString(R.string.about_ipgateway), this.getString(R.string.about_message));
     		//the egg
@@ -171,7 +177,6 @@ public class MainActivity extends Activity {
     				content = new String(byteArray, "GBK");
             	}
 			} catch (UnsupportedEncodingException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
             return content;
@@ -207,14 +212,12 @@ public class MainActivity extends Activity {
         				DialogInterface.OnClickListener ok_listener = new DialogInterface.OnClickListener() {
 							@Override
 							public void onClick(DialogInterface arg0, int arg1) {
-								// TODO Auto-generated method stub
 								disconnectAll = true;
 							}
         				};
         				DialogInterface.OnClickListener cancel_listener = new DialogInterface.OnClickListener() {
 							@Override
 							public void onClick(DialogInterface arg0, int arg1) {
-								// TODO Auto-generated method stub
 								disconnectAll = false;
 							}
         				};
@@ -354,6 +357,79 @@ public class MainActivity extends Activity {
         	
 	        messageArea.loadData(message, "text/html; charset=UTF-8", null);
        }
-    } 
+    }
     
+    public void doLogoutAll() {
+    	
+    	String message = MainActivity.this.getString(R.string.logging_out_all);
+        messageArea.loadData(message, "text/html; charset=UTF-8", null);
+
+    	String username = editUser.getText().toString();
+    	String password = editPass.getText().toString();
+    	
+    	int range = 2;
+	    if (((CheckBox) MainActivity.this.findViewById(R.id.checkBox_global)).isChecked()) {
+	    	range = 1;
+	    }
+    	String logoutAllUrl = "https://its.pku.edu.cn:5428/ipgatewayofpku?uid=" + username + "&password=" + password + "&operation=disconnectall&range=" + range + "&timeout=2";
+    	new IPGatewayLogoutAllTask().execute(logoutAllUrl);
+    }
+    
+    private class IPGatewayLogoutAllTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... urls) {   
+            byte[] byteArray = HttpAccessor.getContentBytesFromUrl(urls[0]);
+            String content = "";
+            try {
+            	if (byteArray != null) {
+    				content = new String(byteArray, "GBK");
+            	}
+			} catch (UnsupportedEncodingException e) {
+				e.printStackTrace();
+			}
+            return content;
+        }
+        
+        // onPostExecute displays the results of the AsyncTask.
+        @Override
+        protected void onPostExecute(String result) {
+        	
+        	boolean remember = ((CheckBox) MainActivity.this.findViewById(R.id.checkBox_remember)).isChecked();
+        	if (!remember) {
+	        	SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(MainActivity.this);
+	        	SharedPreferences.Editor editor = settings.edit();
+	        	editor.remove("username");
+	        	editor.remove("password");
+	        	editor.commit();
+	            editUser.setText("");
+	            editPass.setText("");
+        	}
+        	
+        	String message = MainActivity.this.getString(R.string.something_wrong);
+        	if (result.isEmpty()) {
+        		message = MainActivity.this.getString(R.string.no_data);
+        	} else {
+        		int start = result.indexOf("<!--IPGWCLIENT_START ");
+        		if (start != -1) {
+        			String success = result.substring(start, start + 32);
+        			if (success.equals("SUCCESS=YES")) {
+        				login = false;
+        			}
+        			
+        			start = result.indexOf("<table");
+                	int end = result.lastIndexOf("</table>") + 8;
+                	if (start != -1) {
+                		String table = result.substring(start, end);
+                		message = table;
+                	}
+        			
+        		} else {
+        			message = MainActivity.this.getString(R.string.no_information);
+        		}
+        	}     
+        	
+	        messageArea.loadData(message, "text/html; charset=UTF-8", null);
+       }
+    }
 }
+
